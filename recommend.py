ab9f41d91aabc1996e21f68c23b7386e68aefd76
skarstein <skarstein@scu.edu>
2018-05-22 19:46:30 -0700

    finished parsing training data

--- recommend.py
+++ recommend.py
@@ -1,17 +1,12 @@
-#import numpy as np
+import pandas as pd
+import numpy as np
 
 def parseData():
-	trainingData = open("train.txt")
-	print ("Name of file:", trainingData.name)
-	print ("Closed or not:", trainingData.closed)
-	print ("Opening mode:", trainingData.mode)
-	users = 200
-	movies = 1000
-	matrix = [[0 for x in range(users)] for y in range(movies)]
-	for x in range(users):
-		for y in range(movies):
-			matrix[x][y] = trainingData.read(1)
-	trainingData.close()
+	matrix = pd.read_csv(
+		filepath_or_buffer = "train.txt",
+		sep = "\t",
+		header = None)
+	print (matrix)
 
 if __name__ == "__main__":
 	parseData()
