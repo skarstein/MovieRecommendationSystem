c21f99db0091f020e6308589ccc5a24eb3ca1b9d
skarstein <skarstein@scu.edu>
2018-05-22 18:58:19 -0700

    "wrote parseData function for parsing training data text file"

--- recommend.py
+++ recommend.py
@@ -1,4 +1,17 @@
-print("Hello World!")
+#import numpy as np
 
-print("Hello World!")
-print("Hello World!")
+def parseData():
+	trainingData = open("train.txt")
+	print ("Name of file:", trainingData.name)
+	print ("Closed or not:", trainingData.closed)
+	print ("Opening mode:", trainingData.mode)
+	users = 200
+	movies = 1000
+	matrix = [[0 for x in range(users)] for y in range(movies)]
+	for x in range(users):
+		for y in range(movies):
+			matrix[x][y] = trainingData.read(1)
+	trainingData.close()
+
+if __name__ == "__main__":
+	parseData()
