bf115ab358a03937dcae38a9bddc23ea67a10be0
skarstein <skarstein@scu.edu>
2018-05-22 22:16:37 -0700

    updated cosine similarity

--- cosineSimilarity.py
+++ cosineSimilarity.py
@@ -2,5 +2,25 @@ import pandas as pd
 import numpy as np
 from recommend import parseData
 
+def findNeighbors(k, matrix):
+	users = pd.read_csv(
+		filepath_or_buffer = "test5.txt",
+		sep = " ",
+		names = ["userID", "movie", "rating"])
+
+	users.groupby("userID")
+
+	print (users)
+
+	#users = users.values
+"""
+	for i in range (8496):
+		if (users[i][2] != 0 && matrix:
+			print("what")
+"""	
+def weightedAverage():
+	return 0
+
 if __name__ == "__main__":
 	matrix = parseData()
+	findNeighbors(10, matrix)
