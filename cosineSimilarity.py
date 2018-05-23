923a7bb6581575a5a157445fe7c56fadcb77e00d
skarstein <skarstein@scu.edu>
2018-05-23 00:42:07 -0700

    cosine similarity mostly implemented

--- cosineSimilarity.py
+++ cosineSimilarity.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 from recommend import parseData
 
 def findNeighbors(k, matrix):
@@ -8,17 +9,36 @@ def findNeighbors(k, matrix):
 		sep = " ",
 		names = ["userID", "movie", "rating"])
 
-	users.groupby("userID")
+	#for i in range(201,301):
+	for i in range(201,202):
+		#create immediate user array
+		a = users[users.userID == i]
+		a = a[a.rating != 0]
+		#print(a)
+		#for each row in training data
+		for j in range(200):
+			#create training user array
+			u = matrix.iloc[j]
+			#print(u)
+			#calculate cosine similarity
+			dotProduct = 0
+			count = 0
+			aSquares = 0
+			uSquares = 0
+			for k in range(len(a)):
+				aRating = a.rating.iloc[k]
+				uRating = u.iloc[a.movie.iloc[k]]
+				if(aRating != 0 and uRating != 0):
+					dotProduct += (aRating * uRating)
+					aSquares += aRating**2
+					uSquares += uRating**2
+			print("dotProduct: ", dotProduct)
+			print("aSquares: ", aSquares)
+			print("uSquares: ", uSquares)
+			cosSimilarity = (dotProduct/(math.sqrt(aSquares)+math.sqrt(uSquares)))
+			print(cosSimilarity)
 
-	print (users)
-	
-
-	#users = users.values
-"""
-	for i in range (8496):
-		if (users[i][2] != 0 && matrix:
-			print("what")
-"""	
+		
 def weightedAverage():
 	return 0
 
